c75939448350e4bd2f699e57f8b374deeae7b186
egorovik <egorovi.k@yandex.ru>
2018-02-04 10:49:57 +0300

    Добавление карты Brutal Legend

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function ready(){
+  var map = L.map('mapid', {
+    crs: L.CRS.Simple
+  });
+  var bounds = [[0,0],[1000,1000]];
+  var image = L.imageOverlay("map/map.jpg", bounds).addTo(map);
+  map.fitBounds(bounds);
 }
 
 $(document).ready(ready);
