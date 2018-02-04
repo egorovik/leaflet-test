7c23fbc52f7b4801214b391617cb5e8a82bbf857
egorovik <egorovi.k@yandex.ru>
2018-02-04 11:15:07 +0300

    Маркер Bladehenge

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function ready(){
   var map = L.map('mapid', {
-    crs: L.CRS.Simple
+    crs: L.CRS.Simple,
+    minZoom: -5
   });
-  var bounds = [[0,0],[1000,1000]];
+  var bounds = [[0,0],[1500,3000]];
   var image = L.imageOverlay("map/map.jpg", bounds).addTo(map);
   map.fitBounds(bounds);
+  
+  var bladehenge = L.latLng([410,1027]);
+  L.marker(bladehenge).addTo(map);
 }
 
 $(document).ready(ready);
