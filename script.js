189d5e6b1ec43e32e698eaa77b1ac60ef73f591d
egorovik <egorovi.k@yandex.ru>
2018-02-04 11:23:12 +0300

    Добавление pos и надписи для маркера Bladehenge

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function pos(x, y){
+  if (L.Util.isArray(x)) {
+    return L.latLng([x[1], x[0]]);
+  }
+  return L.latLng([y, x]);
+}
+
 function ready(){
   var map = L.map('mapid', {
     crs: L.CRS.Simple,
@@ -7,8 +14,8 @@ function ready(){
   var image = L.imageOverlay("map/map.jpg", bounds).addTo(map);
   map.fitBounds(bounds);
   
-  var bladehenge = L.latLng([410,1027]);
-  L.marker(bladehenge).addTo(map);
+  var bladehenge = pos(1027, 410);
+  L.marker(bladehenge).addTo(map).bindPopup("Bladehenge");
 }
 
 $(document).ready(ready);
